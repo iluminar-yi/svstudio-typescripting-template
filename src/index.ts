9769928be6b0691dcb5c0eb7336e290923e67e1a
Iluminar <yi.iluminar@gmail.com>
2020-08-02 01:44:36 -0700

    Update demo to reflect latest framework implementation

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,19 @@ import { FrameworkEnvironment, SVScript, SVScriptFactory } from '../framework/ty
 import packageJsonClientInfo from './package-json-client-info';
 
 const svScriptFactory: SVScriptFactory = ({
-  SV: { showMessageBox, setTimeout },
+  context,
+  svSystem: { showMessageBox, setTimeout },
+  utils: { getTimePointInSeconds },
   log,
 }: FrameworkEnvironment): SVScript => {
   return {
     getClientInfo: packageJsonClientInfo,
     async main(): Promise<void> {
       await showMessageBox('Greetings', 'Hello world!');
-      log.info('Greetings displayed');
+      log.debug('Greetings displayed');
+      log.info(
+        `Current project duration is ${context.duration} blicks, or ${getTimePointInSeconds(context.duration)} seconds`,
+      );
       setTimeout(3000, (): void => {
         showMessageBox('Farewell', 'Bye world!');
       });
