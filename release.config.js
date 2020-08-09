29ec2e0f65207a54fb697a6f4061797d7c41819a
Iluminar <yi.iluminar@gmail.com>
2020-08-09 14:48:22 -0700

    fix: Add GitHub Actions configurations

---
+++ release.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  branches: [
+    {
+      name: 'master',
+    },
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+  ],
+};
