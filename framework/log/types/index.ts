edbf697de7a0a3bbd1be7ab43138daf934f810ed
Iluminar <yi.iluminar@gmail.com>
2020-08-12 21:21:06 -0700

    docs: Add documentations in framework/log/types

--- framework/log/types/index.ts
+++ framework/log/types/index.ts
@@ -1,6 +1,36 @@
+/**
+ * A logger interface
+ */
 export interface Logger {
+  /**
+   * Log a message at the DEBUG level.
+   *
+   * @param message - Message body to log.
+   * @param args - Additional data to append to the log message.
+   */
   debug(message: string, ...args: unknown[]): void;
+
+  /**
+   * Log a message at the INFO level.
+   *
+   * @param message - Message body to log.
+   * @param args - Additional data to append to the log message.
+   */
   info(message: string, ...args: unknown[]): void;
+
+  /**
+   * Log a message at the WARNING level.
+   *
+   * @param message - Message body to log.
+   * @param args - Additional data to append to the log message.
+   */
   warning(message: string, ...args: unknown[]): void;
+
+  /**
+   * Log a message at the ERROR level.
+   *
+   * @param message - Message body to log.
+   * @param args - Additional data to append to the log message.
+   */
   error(message: string, ...args: unknown[]): void;
 }
